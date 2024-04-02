c3a2a219810553b8307ad1380d351d61e8f968de
Ahmed Basuony <A.Basuony.social@gmail.com>
2024-04-02 07:14:27 +0200

    update some features

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 
 const categoryRoutes = require("./routes/categoryRoute");
-
+const ApiError = require("./utils/apiError");
 const dbConnection = require("./config/database");
+
 dotenv.config({ path: "config.env" });
 // Connect with db
 dbConnection();
@@ -18,7 +19,6 @@ app.use(express.json()); //  for parsing body application/json => req.body
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev")); // Log HTTP requests in development 'dev' mode relevant information about the HTTP requests, such as the request method, request URL, response status, response time, and response headers
   // ex: GET /api/v1/categories 200 155.634 ms - 755
-  //   console.log("mode:", process.env.NODE_ENV);
 }
 
 // Mount Routes
@@ -27,8 +27,11 @@ app.use("/api/v1/categories", categoryRoutes); // localhost:3000/api/v1/categori
 app.all("*", (req, res, next) => {
   // create error and send it to error handling middleware
   // create  a response error to send data back to the client
-  const err = new Error(`Can't find this  route ${req.originalUrl}`);
-  next(err.message);
+  // const err = new Error(`Can't find this  route ${req.originalUrl}`);
+  // next(err.message);
+
+  // instead we create a class to predict errors
+  next(new ApiError(`Can't find this  route ${req.originalUrl}`, 400));
 });
 
 // Global Error Handling Middleware
