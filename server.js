ed43f487cda233e047b663e1edacdf2b648ddce6
Ahmed Basuony <A.Basuony.social@gmail.com>
2024-04-03 23:51:34 +0200

    Global error handling

--- server.js
+++ server.js
@@ -24,19 +24,24 @@ if (process.env.NODE_ENV === "development") {
 // Mount Routes
 app.use("/api/v1/categories", categoryRoutes); // localhost:3000/api/v1/categories
 
+// handles requests to non-existent routes
+// if the route is not found
 app.all("*", (req, res, next) => {
-  // create error and send it to error handling middleware
-  // create  a response error to send data back to the client
-  // const err = new Error(`Can't find this  route ${req.originalUrl}`);
-  // next(err.message);
-
-  // instead we create a class to predict errors
   next(new ApiError(`Can't find this  route ${req.originalUrl}`, 400));
 });
 
 // Global Error Handling Middleware
+// handles all errors in the application
 app.use((err, req, res, next) => {
-  res.status(400).json(err);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack, // where the error and details
+  });
 });
 
 const PORT = process.env.PORT || 3000;
